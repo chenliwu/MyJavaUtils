a84cc85c3b4a2e706dc1ae147d6a80a68350b67e
BYTTER\chenlw <chenlw@bytter.com>
2019-05-06 18:10:21 +0800

    JAVA8特性练习

---
+++ src/com/chenliwu/java/utils/demo/jdk8/TestStringJoiner.java
@@ -0,0 +1,48 @@
+package com.chenliwu.java.utils.demo.jdk8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+/**
+ * 描述:
+ *
+ * @author chenlw
+ * @create 2019-05-06 16:51
+ */
+public class TestStringJoiner {
+
+    public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test1() {
+        List<String> list = Arrays.asList("A", "B", "C", "D");
+        //String result=  list.stream().collect(Collectors.joining("-"));
+        String result = list.stream().collect(Collectors.joining(" "));
+        System.out.println(result);
+    }
+
+    public static void test2() {
+        String[] strings = new String[]{"A", "B", "C", "D"};
+        StringJoiner joiner = new StringJoiner(".");
+        Arrays.stream(strings).map(i -> i.toString()).forEach(joiner::add);
+        System.out.println(joiner.toString());
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0, sSize = strings.length; i < sSize; i++) {
+            if(i == sSize-1){
+                sb.append(strings[i]);
+                break;
+            }else{
+                sb.append(strings[i]).append(".");
+            }
+        }
+        System.out.println();
+        System.out.println(sb.toString());
+
+    }
+
+}
